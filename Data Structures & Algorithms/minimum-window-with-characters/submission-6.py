7b2fc38f6a8e62c7f9eefbc1365582c36bc717f0
Lulu Wang <32177380+ShenyiQinxin@users.noreply.github.com>
2026-08-07 00:43:36 -0400

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need_map = Counter(t)
+        have_map = defaultdict(int)
+        have = 0
+        min_sub_len = float('inf')
+        substring = ''
+        l=0
+
+        for r in range(len(s)):
+            if s[r] in need_map:
+                have_map[s[r]] += 1
+                if have_map[s[r]] == need_map[s[r]]:
+                    have+=1
+
+            while have == len(need_map):
+                if r-l+1 < min_sub_len:
+                    min_sub_len = r-l+1
+                    substring = s[l:r+1]
+          
+                if s[l] in need_map:
+                    have_map[s[l]] -= 1
+                    # drop *have* when not match with the *need*
+                    if have_map[s[l]] < need_map[s[l]]:
+                        have -= 1
+                l+=1
+ 
+        return substring
+
+        
