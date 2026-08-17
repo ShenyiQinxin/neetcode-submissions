8b9005b145e6a44c401619b11a44f1a32920514a
Lulu Wang <32177380+ShenyiQinxin@users.noreply.github.com>
2026-08-17 02:05:04 -0400

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        # dp[i] is max of rob $$$ from house 0 through i
+        # choice rob i or not rob i
+        def robLinear(nums):
+            if len(nums) == 1:
+                return nums[0]
+            if len(nums) == 2:
+                return max(nums[0], nums[1])
+
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+            for i in range(2, len(nums)):
+                dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+            return dp[-1]
+            
+        
+        if len(nums[:-1]) == 0 or len(nums[1:]) == 0:
+            return nums[0]
+        else:
+            return max(robLinear(nums[:-1]), robLinear(nums[1:]))
+     
+
+
+
+      
+        
+        
