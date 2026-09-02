61f2c8d9d05eb29d23fbc46dab67c361eb2c60e8
Lulu Wang <32177380+ShenyiQinxin@users.noreply.github.com>
2026-09-02 11:44:41 -0400

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n+1)
+        dp[0] = True # seed the 
+
+        for end in range(1, n+1):
+            for start in range(end):
+                if dp[start] and s[start:end] in wordDict:
+                    dp[end] = True
+        return dp[-1]        
