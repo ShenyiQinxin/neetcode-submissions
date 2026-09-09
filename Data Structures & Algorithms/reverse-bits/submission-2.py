b91a29579ce8d31cb20208afa3a3a11618c0253f
Lulu Wang <32177380+ShenyiQinxin@users.noreply.github.com>
2026-09-09 18:19:23 -0400

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res  = 0
+        for _ in range(32):
+            res = res << 1
+            res = res | (n & 1)
+            n = n // 2
+        return res
+        
