f494bc9a646b31e3aaa4efb5cf1adc403ed248b2
Lulu Wang <32177380+ShenyiQinxin@users.noreply.github.com>
2026-09-21 23:27:46 -0400

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.py
@@ -0,0 +1,31 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        visited = {}
+
+        def dfs(node):
+            if not node:
+                return None
+
+            if node in visited:
+                return visited[node]
+
+            copy = Node(node.val)
+            visited[node] = copy
+            for nei in node.neighbors:
+                copy_nei = dfs(nei)
+                copy.neighbors.append(copy_nei)
+            return copy
+
+        return dfs(node)
+        
